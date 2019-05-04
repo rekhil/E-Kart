2b34781aaa894c51dcf0dd38f0611c4e9efa0a88
Rekhil Das <33475238+xrekhil@users.noreply.github.com>
2019-05-05 00:18:08 +0530

    add back end folder

--- e-kart/src/components/ProductDetails.js
+++ e-kart/src/components/ProductDetails.js
@@ -1,19 +1,30 @@
 import React, { Component } from 'react';
-import Img from 'react-image'
+import Img from 'react-image';
+import axios from 'axios';
 
 class ProductDetails extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            product: { productId: 1, productName: 'Product 1', productImage: 'https://m.media-amazon.com/images/I/71YHXM06IcL._AC_UL320_.jpg' }
+            product: { productId: 1, productName: 'Product 1', productImage: 'https://m.media-amazon.com/images/I/71YHXM06IcL._AC_UL320_.jpg' },
+            isLoading: true
         }
     }
 
-    // componentDidMount() {
-    //     console.log(this.props.match.params.id)
-    //     const product = this.state.productList.find(s => s.productId === this.props.match.params.id)
-    //     this.setState({ product: product })
-    // }
+    componentDidMount() {
+        // this.fetchProductDetails();
+    }
+
+    fetchProductDetails() {
+        axios.get("url_here")
+            .then(response => {
+                this.setState({
+                    product: response.data,
+                    isLoading: false
+                });
+            })
+            .catch(error => this.setState({ error, isLoading: false }));
+    }
 
     render() {
         return (
